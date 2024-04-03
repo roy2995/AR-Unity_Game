c702f408b9f897580e9a2a637e281ab316f270d6
Roederick Esquivel <roderickenrique42@gmail.com>
2024-04-03 02:50:13 -0500

    QR code Generator

---
+++ AR_project/Assets/Scripts/QR Code/QRcodeGenerator.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.QrCode;
+
+
+public class QRcodeGenerator : MonoBehaviour
+{
+    [Header("QR Code Generator")]
+    [SerializeField] private GameObject qrPrefab;
+    [SerializeField] private string qrString;
+
+    [Header("QR Code Settings")]
+    [SerializeField] private RawImage imageReceiver;
+    private Texture2D storedEncodedTexture;
+
+    private void Start()
+    {
+        storedEncodedTexture = new Texture2D(256, 256);
+    }
+
+    private Color32[] Encode(string textToEncoding, int width, int height)
+    {
+        BarcodeWriter writer = new BarcodeWriter
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = height,
+                Width = width
+            }
+        };
+        return writer.Write(textToEncoding);
+    }
+
+    public void OnClickEncode()
+    {
+        EncodeTextToQR();
+    }
+
+    private void EncodeTextToQR()
+    {
+        if (storedEncodedTexture == null)
+        {
+            Debug.LogError("String value it's null");
+            // string textWrite = string.IsNullOrEmpty(qrString)? "Null" : qrString;
+        }
+
+        Color32[] pixelTexture = Encode(qrString, storedEncodedTexture.width, storedEncodedTexture.height);
+        storedEncodedTexture.SetPixels32(pixelTexture);
+        storedEncodedTexture.Apply();
+
+        imageReceiver.texture = storedEncodedTexture;
+
+    }
+
+}
